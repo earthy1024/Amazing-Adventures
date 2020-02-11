d6aa385d105f5e49b7fa78656a2e2aa92d42bf26
prithvi4 <prithvi4@illinois.edu>
2020-02-10 21:32:53 -0600

    Added first 3 simple tests to check functionality.

--- src/main/java/student/Main.java
+++ src/main/java/student/Main.java
@@ -1,9 +1,19 @@
 package student;
 
+import com.fasterxml.jackson.core.JsonParser;
+
+
 import student.adventure.Adventure;
 
 public class Main {
     public static void main(String[] args) {
         // TODO: Complete this method.
+        /*
+        Methods needed:
+        1. Function for game start
+        2. Function for user input
+        3. JSON function for custom map
+        4. Function for regular game activity
+         */
     }
 }
--- src/test/java/student/adventure/AdventureTest.java
+++ src/test/java/student/adventure/AdventureTest.java
@@ -1,21 +1,56 @@
 package student.adventure;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import com.sun.xml.internal.bind.v2.util.ByteArrayOutputStreamEx;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class AdventureTest {
+    Adventure adventureGame;
+
     @Before
     public void setUp() {
         // This is run before every test.
+       Adventure adventureGame = new Adventure();
     }
 
     @Test
     public void sanityCheck() {
         // TODO: Remove this unnecessary test case.
-        assertThat("CS 126: Software Design Studio", CoreMatchers.containsString("Software"));
+
+    }
+
+    @Test
+    public void testJSONstart() {
+        assertEquals("MatthewsStreet", gameInitialization());
+    }
+
+    public void testJSONend() {
+        assertEquals("Siebel1314", gameEnd());
+    }
+
+    public void testCorrectInput() {
+        String testInput = "East\nNorth\nSouth\nExit";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(testInput.getBytes());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        adventureGame.gameInitialization();
+        String correctMessage = "You are on Matthews, outside the Siebel Center\n" +
+                "Your journey begins here\n" +
+                "From here, you can go: East\nYou are in the west entry of Siebel Center. You can see the elevator, the ACM office, and hallways to the north and east.\n" +
+                "From here, you can go: West, Northeast, North, or East\nYou are in the north hallway. You can see Siebel 1105 and Siebel 1112. From here, you can go: South\nYou are in the west entry of Siebel Center. You can see the elevator, the ACM office, and hallways to the north and east" +
+                "From here, you can go: West, Northeast, North, or East";
+
+        System.setOut(new PrintStream(outputStream));
+
+        assertEquals(correctMessage, outputStream.toString());
+
+
     }
 }
